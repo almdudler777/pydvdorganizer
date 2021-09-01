635102bca8a1fc1ee1467560ca38408b8d0ab53a
almdudler777 <github@almdudler777.de>
2021-09-01 21:18:46 +0200

    change the appid domain

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import ctypes
 
 if sys.platform.startswith('win32'):
     # make windows show the correct icon even if hosted through pythonw.exe
-    myappid = 'almdudler777.de.pydvdorganizer.0815' # arbitrary string
+    myappid = 'de.almdudler777.pydvdorganizer'
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 translator = None
